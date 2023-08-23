8210af7a6e636f61d03ada0b5ad91aedf950f449
Turco777 <agusalmona@gmail.com>
2023-08-23 20:00:16 -0300

    cambios

--- src/routes/transactionsRouter.js
+++ src/routes/transactionsRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const transactionsController = require("../controllers/TransactionsController.js");
 
 // Rutas para CRUD de planes
-router.get("/", transactionsController);
-router.get("/:id", transactionsController);
-router.post("/", transactionsController);
-router.put("/:id", transactionsController);
-router.delete("/:id", transactionsController);
+router.get("/", transactionsController.getAllTransactions);
+router.get("/:id", transactionsController.getTransactionById);
+router.post("/", transactionsController.createTransaction);
+router.put("/:id", transactionsController.updateTransaction);
+router.delete("/:id", transactionsController.deleteTransaction);
 
 module.exports = router;
