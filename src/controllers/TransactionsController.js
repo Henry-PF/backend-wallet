b159b02f4abf57e903544c21eeeeac8b323401f2
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-09-07 00:12:41 -0400

    transacciones funcionando

--- src/controllers/TransactionsController.js
+++ src/controllers/TransactionsController.js
@@ -1,9 +1,10 @@
-const { Transactions } = require("../db.js");
+const { registro_transferencia, usuarios, usuarios_bolsillo } = require("../db.js");
+const { Op } = require("sequelize");
 
 const transactionsController = {
   getAllTransactions: async (req, res) => {
     try {
-      const transactions = await Transactions.findAll();
+      const transactions = await registro_transferencia.findAll();
       res.status(200).json(transactions);
     } catch (error) {
       console.error(error);
@@ -14,7 +15,7 @@ const transactionsController = {
   getTransactionById: async (req, res) => {
     const { id } = req.params;
     try {
-      const transaction = await Transactions.findByPk(id);
+      const transaction = await registro_transferencia.findByPk(id);
       if (transaction) {
         res.status(200).json(transaction);
       } else {
@@ -31,15 +32,60 @@ const transactionsController = {
       req.body;
 
     try {
-      const newTransaction = await Transactions.create({
-        concepto,
-        id_usuario_emisor,
-        monto,
-        id_usuario_receptor,
-        fecha,
-      });
-
-      res.status(201).json(newTransaction);
+      //verificar usuario emisor
+      let usuario_emisor = await usuarios.findByPk(id_usuario_emisor);
+      if (usuario_emisor) {
+        let usuario_receptor = await usuarios.findByPk(id_usuario_receptor);
+        if (usuario_receptor) {
+          let saldo_usuario_emi = await usuarios_bolsillo.findOne(
+            {
+              where: { 
+                id_usuario: { 
+                  [Op.eq]: usuario_emisor.id 
+                } 
+              },
+              include: [{ model: usuarios }]
+            });
+            if(saldo_usuario_emi){
+              if(parseFloat(saldo_usuario_emi.saldo)>=parseFloat(monto)){
+                let newTransaction = await registro_transferencia.create({
+                  concepto,
+                  id_usuario_emisor,
+                  monto,
+                  id_usuario_receptor,
+                  fecha,
+                });
+                if(newTransaction){
+                  let new_saldo = {saldo:parseFloat(saldo_usuario_emi.saldo)-parseFloat(monto)}
+                  await saldo_usuario_emi.update(new_saldo);
+                  let saldo_usuario_rep = await usuarios_bolsillo.findOne({
+                    where: { 
+                      id_usuario: { 
+                        [Op.eq]: usuario_receptor.id 
+                      } 
+                    },
+                    include: [{ model: usuarios }]
+                  });
+                  if(saldo_usuario_rep){
+                    let new_saldo_rep = {saldo:parseFloat(saldo_usuario_rep.saldo)+parseFloat(monto)}
+                    await saldo_usuario_rep.update(new_saldo_rep);
+                    res.status(201).json({ message: "Transaccion exitosamente" });
+                  }else{
+                    res.status(500).json({ message: "Error al obtener el saldo del receptor" });
+                  }
+                }else{
+                  res.status(500).json({ message: "Transaccion no realiza" });
+                }
+              }else{
+                res.status(500).json({ message: "Saldo insuficiente" });
+              }
+            }
+        }else{
+          res.status(500).json({ message: "Error usuario receptor no encontrado" });
+        }
+      }else{
+        res.status(500).json({ message: "Error usuario emisor no encontrado" });
+      }
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Error al crear la transaccion" });
@@ -51,7 +97,7 @@ const transactionsController = {
     const { concepto, id_usuario_emisor, monto, id_usuario_receptor, fecha } =
       req.body;
     try {
-      const transaction = await Transactions.findByPk(id);
+      const transaction = await registro_transferencia.findByPk(id);
       if (transaction) {
         await transaction.update({
           concepto,
@@ -75,7 +121,7 @@ const transactionsController = {
   deleteTransaction: async (req, res) => {
     const { id } = req.params;
     try {
-      const transaction = await Transactions.findByPk(id);
+      const transaction = await registro_transferencia.findByPk(id);
       if (transaction) {
         await transaction.destroy();
         res.status(200).json({ message: "Transaccion eliminada exitosamente" });
